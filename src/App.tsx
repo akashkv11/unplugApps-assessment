5f76c4b9b7637fd8c880ba671fc7a056726e5583
akashkv22 <150666293+akashkv22@users.noreply.github.com>
2024-10-06 18:00:39 +0530

    Initial commit and page started

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Sales from "./pages/Sales";
+
+function App() {
+  return (
+    <>
+      <Sales />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Sales.tsx
@@ -0,0 +1,13 @@
+import Details from "../components/Details";
+import Header from "../components/Header";
+
+const Sales = () => {
+  return (
+    <>
+      <Header />
+      <Details />
+    </>
+  );
+};
+
+export default Sales;
